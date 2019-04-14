85c24f68b4338c7d2f03a89435a53a99b79b9e30
KangWon LEE <kangwon.lee@kpu.ac.kr>
2019-04-14 22:19:24 +0900

    test_get_repo_comments_public() : check scheme & id

--- tests/test_github.py
+++ tests/test_github.py
@@ -1,3 +1,5 @@
+import urllib.parse as up
+
 import pytest
 
 import pyapi
@@ -5,4 +7,11 @@ import pyapi
 
 def test_get_repo_comments_public():
     result = pyapi.get_repo_comments_public('octocat', 'Hello-World')
-    
+    # https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.at.html#pandas.DataFrame.at
+
+    id = result.at[result.index[-1], 'id']
+    url = result.at[result.index[-1], 'url']
+    parse = up.urlparse(url)
+
+    assert 'https' == parse.scheme
+    assert parse.path.endswith(str(id))
