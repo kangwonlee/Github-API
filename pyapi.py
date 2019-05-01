d1de125acdb26e102747346f0e4811f6287cfadb
KangWon LEE <kangwon.lee@kpu.ac.kr>
2019-05-01 22:27:46 +0900

    process_todo_list_json_file() : consider duplicate_counter

--- pyapi.py
+++ pyapi.py
@@ -610,7 +610,8 @@ def process_todo_list_json_file(*todo_list_json_filename_list, b_verbose=True):
         if retry_list:
             if b_verbose:
                 print(
-                    f"# sent messages == {len(response_list) - len(retry_list)}\n"
+                    f"# sent messages == {len(response_list) - len(retry_list) - duplicate_counter}\n"
+                    f"# duplicate     == {duplicate_counter}\n"
                     f"len(retry_list) == {len(retry_list)}\n"
                 )
 
